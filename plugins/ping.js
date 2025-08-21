aa2a3555b98e12531983de446eeebe3ba190fd16
skc-sketch112 <chunnachud@gmail.com>
2025-08-21 12:19:16 +0530

    Update ping.js

--- plugins/ping.js
+++ plugins/ping.js
@@ -1,7 +1,8 @@
 module.exports = {
     name: "ping",
-    description: "Check if bot is alive",
-    run: async (sock, from) => {
-        await sock.sendMessage(from, { text: "🏓 Pong! Bot is alive." });
+    description: "Replies with pong",
+    run: async (sock, from, args, msg) => {
+        console.log("⚡ Ping command triggered!");
+        await sock.sendMessage(from, { text: "🏓 pong!SOURAV_MD V 4.10.15" }, { quoted: msg });
     }
 };
